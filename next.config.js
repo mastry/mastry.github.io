a06788b304a91a643cf13d70279762e7f9804060
Jeff Mastry <jeff@mastry.net>
2023-01-15 10:52:08 -0500

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: '/mastry',
   reactStrictMode: true,
   swcMinify: true,
-  experimental: {
-    images: {
-      unoptimized: true
-    }
+
+  images: {
+    unoptimized: true
   }
 }
 
